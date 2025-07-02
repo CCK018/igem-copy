2aa1d5a76db969646ef5233d0bb908e0bd6a1072
CCK018 <colinconnor06@gmail.com>
2025-07-02 03:22:10 -0400

    Update Component1.tsx

--- src/components/Component1.tsx
+++ src/components/Component1.tsx
@@ -2,8 +2,39 @@ import React from "react"
 
 export default function Component1 () {
     return (
-        <div>
-            Component 1 placeholder
+        <div
+          className="contentBox"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "20px",
+            flexWrap: "wrap",
+          }}
+        >
+          <img
+            className="image"
+            src="https://placehold.co/600x400"
+            alt="image2"
+            style={{
+              height: "150px",
+              objectFit: "cover",
+              width: "300px"
+            }}
+          />
+          <p
+            className="textBox"
+            style={{
+              backgroundColor: "darkgreen",
+              color: "white",
+              height: "150px",
+              width: "300px"
+            }}
+          >
+            lorem
+          </p>
         </div>
-    )
+    );
 }
